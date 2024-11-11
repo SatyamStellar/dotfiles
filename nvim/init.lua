ab9d35558c32067582ca00278f255d4264ffe8ef
satymstellar <styam321tomar@gmail.com>
2024-11-11 19:15:01 +0530

    Adding again

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-require('config.globals')
-require("config")
-=======
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
->>>>>>> 749e61d (Updating my config files in linux)
