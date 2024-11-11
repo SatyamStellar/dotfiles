ae6b85ddaacee11f02ccc275f7265aabb9194fa2
satymstellar <styam321tomar@gmail.com>
2024-11-11 18:49:07 +0530

    Adding again

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 local opt = vim.opt
 
 opt.number = true                          -- Make line numbers default (default: false)
@@ -48,3 +49,17 @@ opt.runtimepath:remove '/usr/share/opt.'   -- Separate Vim plugins from Neovim i
 opt.guicursor =
 "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 opt.encoding = "UTF-8"
+=======
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.conceallevel = 0
+opt.cmdheight = 0
+
+vim.g.root_spec = { "cwd" }
+vim.g.omni_sql_no_default_maps = 1
+vim.g.python3_host_prog = "/usr/bin/python3"
+>>>>>>> 749e61d (Updating my config files in linux)
